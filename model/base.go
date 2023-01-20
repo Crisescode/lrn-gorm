41a5f25763eb76f11a4b940cc82b4ecb4dfdf54b
Crisescode <crise.zhaopp@gmail.com>
2023-01-20 10:25:37 +0800

    u

--- model/base.go
+++ model/base.go
@@ -6,7 +6,7 @@ import (
 )
 
 func DBClient() (*gorm.DB, error) {
-	dsn := "pdrtwd_user:123456@tcp(10.114.31.71:13310)/ppdai_pdrtwd?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "user:password@tcp(ip:port)/ppdai_pdrtwd?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
